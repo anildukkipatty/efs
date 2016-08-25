4a1ae4f13d18e8a2e42bc2842d11e34127858e4f
Anil Dukkipatty <anildukkipatty@gmail.com>
2016-08-25 16:02:45 +0530

    1. Enabled cors

--- app.js
+++ app.js
@@ -31,7 +31,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 
